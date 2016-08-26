6ad043781cbf4a1641573a0d31b0653a04439f8c
Christian <christianvaldez36@hotmail.com>
2016-08-26 10:10:11 -0700

    Actual Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function Person(name,age) {
+	this.name = name;
+	this.age = age;
+}
+
+var chris = new Person("Christian",21);
+console.log(chris.name);
+
+var fullName = "Christian Valdez";
+console.log(fullName.length);
+
